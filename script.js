382a49c840ee60127a7e8558ab25994c6615d3d9
NICO <annicolas2@gmail.com>
2025-04-16 12:13:13 -0500

    Tarjetas ajustadas

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+//Contact
 document.getElementById("contact-send").addEventListener('click', () => {
-    event.preventDefault;
-    
+    event.preventDefault();
+    const input = document.getElementById("visitor-name");
+    const nombre = input.value.trim(); //Quita los espacios en blanco al inicio
+    const email = document.getElementById("visitor-email");
+    const phone = document.getElementById("visitor-number");
+
+    if (nombre){
+        const mensaje = document.createElement('p');
+        mensaje.id = 'contact-message'
+        mensaje.textContent = `${nombre}, ¡Estaremos en contacto pronto!`;
+        document.getElementById("contact-links").appendChild(mensaje);
+        //Form back to zero
+        input.value = '';
+        email.value = '';
+        phone.value = '';
+    }
 })
